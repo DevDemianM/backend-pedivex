e2393e30bf7551e1d95c2ca003b5dd5b663d0fda
Andrexix24 <andrexfelipex07@gmail.com>
2024-08-11 15:01:05 -0500

    se edita validaciones de permisos

--- src/routes/rolePermissionRoutes.js
+++ src/routes/rolePermissionRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const rolePermissionController = require('../controllers/rolePermissionController');
-const validatePermission = require('../middlewares/validatePermisssion');
 
 const router = express.Router();
 
 router.get('/', rolePermissionController.getAllRolePermissions);
 router.get('/:id', rolePermissionController.getRolePermissionById);
-router.post('/',  validatePermission,  rolePermissionController.createRolePermission);
-router.put('/:id', validatePermission,  rolePermissionController.updateRolePermission);
+router.post('/',  rolePermissionController.createRolePermission);
+router.put('/:id',  rolePermissionController.updateRolePermission);
 router.delete('/:id', rolePermissionController.deleteRolePermission);
 
 module.exports = router;
