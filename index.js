13d2053e891c9511f2950a6bcfb856fdc8a9429c
Jh0nC <Jh0nC>
2024-08-14 15:22:21 -0500

    se organiza para que no cree los constraints al ejecutarse

--- index.js
+++ index.js
@@ -46,9 +46,26 @@ app.use('/user', userRoutes);
 
 const port = process.env.SERVER_PORT || 3000;
 
-const startServer = async () => {
+/// descomenta o comenta segun necesidad
+
+
+// async para sincronizar la BD al iniciar
+
+// const startServer = async () => {
+//   try {
+//     await connectDb();
+//     app.listen(port, () => {
+//       console.log(`Server is running on port ${port}`);
+//     });
+//   } catch (error) {
+//     console.error('Error starting the server:', error);
+//   }
+// };
+
+// sin el async solo para hacer funcionar el server
+
+const startServer = () => {
   try {
-    await connectDb();
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
