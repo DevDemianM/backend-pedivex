fbeb09a4a92df3c7d12eef96f63ccfb6f723a510
McDamian12 <damimono4@gmmail.com>
2024-08-19 11:19:01 -0500

    DM- Modulo de Auth con JWT y nodemailer

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const { validateRegistration } = require('../middlewares/authValidation');
 
 const router = express.Router();
 
-// Rutas de autenticación
-router.post('/register', authController.register);
+router.post('/register', validateRegistration, authController.register);
 router.post('/login', authController.login);
-router.post('/recover-password', authController.recoverPassword);
+router.post('/recover-password', authController.recoverPassword); // Iniciar recuperación
+router.post('/reset-password/:token', authController.confirmResetPassword); // Restablecer contraseña
 
 module.exports = router;
