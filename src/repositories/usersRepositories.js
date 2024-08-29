7a0017f2473e0cd64a351051f79c5333b2dc5129
Jh0nC <Jh0nC>
2024-08-29 15:18:10 -0500

    se corrige la funcionalidad de get clients y employees

--- src/repositories/usersRepositories.js
+++ src/repositories/usersRepositories.js
@@ -1,43 +1,55 @@
 const { models } = require('../models');
 const { Op } = require('../config/database');
 const getAllUsers = async () => {
-    return await models.User.findAll({
-        include: [models.Role]
-    });
+  return await models.User.findAll({
+    include: [models.Role]
+  });
 };
 
 const getUserById = async (id) => {
-    return await models.User.findByPk(id), {
-        include: [models.Role]
-    };
+  return await models.User.findByPk(id, {
+    include: [models.Role]
+  });
 };
 
-const getAllClientUsers = async () => models.User.findAll(/* { where: { idRole: 6 } } */);
+const getAllClientUsers = async () => {
+  return await models.User.findAll({
+    where: {
+      idRole: 6
+    }
+  });
+}
 
-const getAllEmployeeUsers = async () => models.User.findAll({ where: { idRole: 7 } });
+const getAllEmployeeUsers = async () => {
+  return await models.User.findAll({
+    where: {
+      idRole: 7
+    }
+  });
+}
 
 const createUser = async (data) => {
-    return await models.User.create(data);
+  return await models.User.create(data);
 };
 
 const updateUser = async (id, data) => {
-    return await models.User.update(data, {
-        where: { id }
-    });
+  return await models.User.update(data, {
+    where: { id }
+  });
 };
 
 const deleteUser = async (id) => {
-    return await models.User.destroy({
-        where: { id }
-    });
+  return await models.User.destroy({
+    where: { id }
+  });
 };
 
 module.exports = {
-    getAllUsers,
-    getUserById,
-    getAllClientUsers,
-    getAllEmployeeUsers,
-    createUser,
-    updateUser,
-    deleteUser
+  getAllUsers,
+  getUserById,
+  getAllClientUsers,
+  getAllEmployeeUsers,
+  createUser,
+  updateUser,
+  deleteUser
 };
--- src/services/usersServices.js
+++ src/services/usersServices.js
@@ -2,48 +2,41 @@ const { Op } = require('sequelize');
 const usersRepository = require('../repositories/usersRepositories');
 
 const getAllUsers = async () => {
-    return await usersRepository.getAllUsers();
+  return await usersRepository.getAllUsers();
 };
 
 const getUserById = async (id) => {
-    return await usersRepository.getUserById(id);
+  return await usersRepository.getUserById(id);
 };
 
 // Función para obtener todos los usuarios clientes
 const getAllClientUsers = async () => {
-    try {
-        return await User.findAll({ where: { idRole: { [Op.eq]: 6 } } });
-    } catch (error) {
-        throw new Error('Error obteniendo usuarios clientes');
-    }
+  return await usersRepository.getAllClientUsers();
 };
 
 // Función para obtener todos los usuarios empleados
 const getAllEmployeeUsers = async () => {
-    try {
-        return await User.findAll({ where: { idRole: { [Op.eq]: 7 } } });
-    } catch (error) {
-        throw new Error('Error obteniendo usuarios empleados');
-    }
+  return await usersRepository.getAllEmployeeUsers();
 };
+
 const createUser = async (data) => {
-    return await usersRepository.createUser(data);
+  return await usersRepository.createUser(data);
 };
 
 const updateUser = async (id, data) => {
-    return await usersRepository.updateUser(id, data);
+  return await usersRepository.updateUser(id, data);
 };
 
 const deleteUser = async (id) => {
-    return await usersRepository.deleteUser(id);
+  return await usersRepository.deleteUser(id);
 };
 
 module.exports = {
-    getAllUsers,
-    getUserById,
-    getAllClientUsers,
-    getAllEmployeeUsers,
-    createUser,
-    updateUser,
-    deleteUser
+  getAllUsers,
+  getUserById,
+  getAllClientUsers,
+  getAllEmployeeUsers,
+  createUser,
+  updateUser,
+  deleteUser
 };
