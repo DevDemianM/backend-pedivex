a75b17dc6009be41581e355caa272a2737c6eaae
Andrexix24 <andrexfelipex07@gmail.com>
2024-09-11 01:27:51 -0500

    se arregla el user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -9,11 +9,6 @@ router.post('/', validateUser, userController.createUser);
 router.put('/:id', validateUser, userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
-<<<<<<< HEAD
-
-=======
-// Nueva ruta para actualizar solo el estado de un usuario
 router.patch('/:id', userController.updateUserState);
->>>>>>> 34d7e6070212b63d5811b35bacea605f1fc42d8b
 
 module.exports = router;
