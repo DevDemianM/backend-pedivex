545eb982a1899a35adbfedbbde99e62ff891a7fe
McDamian12 <damimono4@gmail.com>
2024-09-25 13:59:51 -0500

    DM: Cambios en la datatable de clientes y empleados

--- src/routes/clientsRoutes.js
+++ src/routes/clientsRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/usersController');
 const router = express.Router();
+const validateUser = require('../middlewares/validateUser')
 
 router.get('/', userController.getAllClientUsers);
 router.get('/:id', userController.getUserById);
+router.put('/:id', validateUser, userController.updateUser);
 
 
 
--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/usersController');
 const router = express.Router();
+const validateUser = require('../middlewares/validateUser')
+
 
 router.get('/', userController.getAllEmployeeUsers);
 router.get('/:id', userController.getUserById);
-
+router.put('/:id', validateUser, userController.updateUser);
 
 module.exports = router;
