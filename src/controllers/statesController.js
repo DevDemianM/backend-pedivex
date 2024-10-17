c95d7a644a38a97618992e9a583fc23fc05fd488
Jh0nC <jhonatancorrales681@gmail.com>
2024-10-17 08:30:28 -0500

    sabra diosito

--- src/controllers/statesController.js
+++ src/controllers/statesController.js
@@ -10,6 +10,46 @@ const getAllStates = async (req, res) => {
   };
 }
 
+const getStateById = async (req, res) => {
+  try {
+    const state = await statesServices.getStateById(req.params.id);
+    sendResponse(res, state);
+  } catch (error) {
+    sendError(res, error);
+  }
+}
+
+const createState = async (req, res) => {
+  try {
+    const state = await statesServices.createState(req.body);
+    sendResponse(res, state);
+  } catch (error) {
+    sendError(res, error);
+  }
+}
+
+const updateState = async (req, res) => {
+  try {
+    const state = await statesServices.updateState(req.param.id, req.body);
+    sendResponse(res, state);
+  } catch (error) {
+    sendError(res, error);
+  }
+}
+
+const deleteState = async (req, res) => {
+  try {
+    const state = await statesServices.deleteState(req.param.id);
+    sendResponse(res, state);
+  } catch (error) {
+    sendError(res, error);
+  }
+}
+
 module.exports = {
-  getAllStates
+  getAllStates,
+  getStateById,
+  createState,
+  updateState,
+  deleteState
 }
--- src/services/statesServices.js
+++ src/services/statesServices.js
@@ -4,6 +4,26 @@ const getAllStates = async () => {
   return await statesrepository.findAllStates();
 }
 
+const getStateById = async (id) => {
+  return await statesrepository.findStateById(id);
+}
+
+const createState = async (name) => {
+  return await statesrepository.createState(data);
+}
+
+const updateState = async (id, name) => {
+  return await statesrepository.updateState(id, name);
+}
+
+const deleteState = async (id) => {
+  return await statesrepository.deleteState(id);
+}
+
 module.exports = {
-  getAllStates
+  getAllStates,
+  getStateById,
+  createState,
+  updateState,
+  deleteState
 }
