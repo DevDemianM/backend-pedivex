9465af428093996f385daba4c0d2808241e0176c
MiguelTabares001 <demisebully@gmail.com>
2024-10-22 21:44:10 -0500

    Ordenes y Pedidos

--- src/repositories/requestRepository.js
+++ src/repositories/requestRepository.js
@@ -13,6 +13,7 @@ const getRequestById = async (id) => {
     });
 };
 
+/* ESTE BLOQUE FUNCIONA PERO SIN EL CALCULO DE TOTAL 
 const createRequest = async (data) => {
     const transaction = await sequelize.transaction();
     try {
@@ -46,6 +47,68 @@ const createRequest = async (data) => {
         return { success: false, error };
     }
 };
+*/
+
+const createRequest = async (data) => {
+    const transaction = await sequelize.transaction();
+    try {
+      const { creationDate, idUser, state, deadLine, stateDate, details } = data;
+  
+      // Obtener precios de los productos desde la tabla de productos
+      const productIds = details.map(detail => detail.idProduct);
+      const products = await models.Product.findAll({
+        where: {
+          id: productIds
+        },
+        transaction
+      });
+  
+      // Calcular el total basado en los precios y cantidades
+      let total = 0;
+      const requestDetails = details.map(detail => {
+        const product = products.find(prod => prod.id === detail.idProduct);
+        const price = product ? parseFloat(product.price) : 0;
+        const subtotal = price * detail.quantity;
+  
+        total += subtotal;
+  
+        return {
+          ...detail,
+          idRequest: null, // Este valor será asignado luego de crear la solicitud
+          subtotal,
+          total: subtotal
+        };
+      });
+  
+      // Crea la solicitud con el total calculado
+      const request = await models.Request.create({
+        creationDate,
+        idUser,
+        total, // Asignar el total calculado
+        state,
+        deadLine,
+        stateDate
+      }, { transaction });
+  
+      // Actualizar el campo idRequest en los detalles y crear los detalles
+      const updatedRequestDetails = requestDetails.map(detail => ({
+        ...detail,
+        idRequest: request.id
+      }));
+      await models.RequestDetail.bulkCreate(updatedRequestDetails, { transaction });
+  
+      // Confirma la transacción
+      await transaction.commit();
+  
+      return { success: true, request };
+    } catch (error) {
+      // Revertir la transacción en caso de error
+      await transaction.rollback();
+      console.error('Error al crear la solicitud y detalles:', error);
+      return { success: false, error };
+    }
+};
+  
 
 const updateRequest = async (id, data) => {
     return await models.Request.update(data, {
